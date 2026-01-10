54d60a1bcabf744963124924139c76f675caa51f
Sai-devops-22 <badesaivenkat@gmail.com>
2026-01-10 14:52:41 +0530

    added

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "latest-84s-remote-state-dev"
+    bucket = "sai-devops-new"
     key    = "roboshop-dev-vpc"
     region = "us-east-1"
     encrypt        = true
